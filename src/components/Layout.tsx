2eb315fbc0c7930da8631867fd431341d62cb587
Edo <edogultom10395@gmail.com>
2025-08-26 20:53:18 +0700

    finished

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <div className="min-h-screen bg-gray-100 text-gray-900">
-      {/* outlet same with slot in RRV6 nested route */}
       <Outlet />
     </div>
   );
