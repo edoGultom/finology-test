b97f7c95fd5b2a032aed65b1ffa5e05ad5dc06c0
Edo <edogultom10395@gmail.com>
2025-08-27 11:02:26 +0700

    refactor:layout and filtering

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -48,7 +48,7 @@ const Index: React.FC = () => {
 
       {/* Main Content */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <div className="space-y-6 flex flex-col">
+        <div className="space-y-6 ">
           {/* Search and Filters */}
           <FilterControls
             filters={filters}
